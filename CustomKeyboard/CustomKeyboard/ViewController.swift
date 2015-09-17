3886d917699d345b4bc0d28aa4a8afe03c3e41b9
geneyoo <geneyoo@berkeley.edu>
2015-09-17 01:01:36 -0700

    updated skeleton

--- CustomKeyboard/CustomKeyboard/ViewController.swift
+++ CustomKeyboard/CustomKeyboard/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
